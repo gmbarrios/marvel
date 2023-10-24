d4410ae3e8c47e2f004a319ef527f8fce4648408
marcelbarrios <gabrielmbarrios@gmail.com>
2023-10-24 08:35:25 -0500

    feat: fix searchbox

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,10 +2,12 @@ import styles from "./searchbar.module.css";
 export const SearchBar = () => {
     return (
         <header>
-        <div className={styles.searchBox}>
-            <div className={styles.logo}></div>
-            <input className={styles.searchInput} type="search" placeholder="Buscar" />
-            Buscar</div>
+            <div>
+                 <div className={styles.searchBox}>
+                     <div className={styles.logo}></div>
+                      <input className={styles.searchInput} type="search" placeholder="Buscar" /*class=""search-input*/ />
+                  </div>
+            </div>
         </header>
     );
   };
